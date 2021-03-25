53bf30aba2c74bc96368349d7cc77208d015ee1b
kuriharakentarou <kkurihara@akane.waseda.jp>
2021-03-25 16:31:52 +0900

    step43完了

--- dezero/functions.py
+++ dezero/functions.py
@@ -3,7 +3,7 @@ from typing import Tuple
 import numpy as np
 
 from dezero.core import Function, Variable, as_variable
-from dezero import utils
+from dezero import utils, cuda
 
 
 class Sin(Function):
@@ -185,3 +185,71 @@ class MeanSquaredError(Function):
 def mean_squared_error(x0, x1):
     return MeanSquaredError()(x0, x1)
 
+
+class Linear(Function):
+    def forward(self, x: np.ndarray, W: np.ndarray, b: np.ndarray):
+        y = x.dot(W)
+        if b is not None:
+            y += b
+        return y
+
+    def backward(self, gy: Variable):
+        x, W, b = self.inputs
+        gb = None if b.data is None else sum_to(gy, b.shape)
+        gx = matmul(gy, W.T)
+        gW = matmul(x.T, gy)
+        return gx, gW, gb
+
+
+def linear(x, W, b=None):
+    return Linear()(x, W, b)
+
+
+def linear_simple(x, W, b=None):
+    x, W = as_variable(x), as_variable(W)
+    t = matmul(x, W)
+    if b is None:
+        return t
+
+    y = t + b
+    t.data = None
+    return y
+
+def sigmoid_simple(x):
+    x = as_variable(x)
+    y = 1 / (1 + exp(-x))
+    return y
+
+
+class Sigmoid(Function):
+    def forward(self, x):
+        xp = cuda.get_array_module(x)
+        # y = 1 / (1 + xp.exp(-x))
+        y = xp.tanh(x * 0.5) * 0.5 + 0.5  # Better implementation
+        return y
+
+    def backward(self, gy):
+        y = self.outputs[0]()
+        gx = gy * y * (1 - y)
+        return gx
+
+
+def sigmoid(x):
+    return Sigmoid()(x)
+
+
+# class ReLU(Function):
+#     def forward(self, x):
+#         xp = cuda.get_array_module(x)
+#         y = xp.maximum(x, 0.0)
+#         return y
+#
+#     def backward(self, gy):
+#         x, = self.inputs
+#         mask = x.data > 0
+#         gx = gy * mask
+#         return gx
+#
+#
+# def relu(x):
+#     return ReLU()(x)
